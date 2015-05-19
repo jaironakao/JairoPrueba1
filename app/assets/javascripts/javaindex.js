67b2d41c4d5e9b339c85627a7617b9ae12fadf4e
sakretsu <sakretsu@warmachine.(none)>
2015-05-19 12:41:15 -0400

    Add Boostrap

--- app/assets/javascripts/javaindex.js
+++ app/assets/javascripts/javaindex.js
@@ -1,5 +1,57 @@
 //alert("Saul petero") ; alert("De sergio")
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    alert( "ready!" );
+/*var dia= ["lunes", "martes","miercoles","jueves","viernes","sabado"];
+	for (var i = 0; i <= dia.length -1 ; i++) {
+		alert( dia[i] );
+	}; ---------------------------------------*/
+ /*for (var i= 1; i <= 18; i++)
+   	alert(i);
+  	if (i=18)  			
+  		alert("Ya eres incogible");+
+ 	----------------------------------------*/
+/*funtion1() ;
+function funtion1(){ 	
+ 	for (var i= 1; i <= 18; i++)
+  
+  	alert(i);
+  	if (i=18)  			
+  		alert("Ya eres incogible");
+ } ---------------------------------------*/
+/*  var suma1 =3;
+  var suma2=7;
+  function sumarnumeros(valor1, valor2 ){
+  	
+  	var resultado = (valor1 + valor2);
+  	alert (resultado)
+  }	
+sumarnumeros(suma1, suma2);-----------------*/
+
+/*parrafo1 = document.getElementById("p1");
+parrafo2 = document.getElementById("p2");
+alert (parrafo1.textContent + parrafo2.textContent)
+-------------------------------------------*/
+/*$( "p" ).hover(function() {
+	alert ("Paso por encima del parrafo")
+});------------------------------------*/
+ 
+// Copia el parrafo cada vez que el cursor pasa por encima
+/*var nuevo= $("#p1"); //el numeral es para hacer referencia al Id
+function clonar(){
+	nuevo.clone().appendTo("body");
+}
+$( "p" ).hover(clonar)*/
+
+/*var colorsito= $("#p1"); //el numeral es para hacer referencia al Id
+function cambiarcolor(){
+	//$( "p" ).css( "color", "red" );
+	//$( "p" ).css( "fontSize", "100px" );
+
+	$("p").css({
+		fontSize: "80px",
+		color: "red"
+		});
+}
+$( "p" ).hover(cambiarcolor)*/
+
 });
