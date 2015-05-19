0e662eb6460b8a61380e4206076fc4c49aadc2c1
sakretsu <sakretsu@warmachine.(none)>
2015-05-19 17:23:23 -0400

    Image responsive

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+get 'welcome/about'
+get 'welcome/contact'
+get 'welcome/home'
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'welcome#home'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
